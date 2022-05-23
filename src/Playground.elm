23abd7a4f12a134843eb3d96480473e877afaf85
maso-soup <53543388+maso-soup@users.noreply.github.com>
2022-05-22 20:41:17 -0400

    Elm Practice

---
+++ src/Playground.elm
@@ -0,0 +1,124 @@
+module Playground exposing (main)
+
+import Html
+import Regex
+
+computeSpeed distance time =
+    distance / time
+
+
+computeTime startTime endTime =
+    endTime - startTime
+
+escapeEarth myVelocity mySpeed fuelStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand  =
+            if fuelStatus == "low" then
+                "Land on droneship"
+
+            else
+                "Land on launchpad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "Stay in orbit"
+
+    else
+        whereToLand
+
+
+add a b =
+    a + b
+
+
+multiply c d =
+    c * d
+
+
+divide e f =
+    e / f
+
+
+
+weekday dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+validateEmail email =
+    let
+        emailPattern =
+            "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+        regex =
+            Maybe.withDefault Regex.never <|
+                Regex.fromString emailPattern
+
+        isValid =
+            Regex.contains regex email
+    in
+    if isValid then
+        ( "Valid email", "green" )
+
+    else
+        ( "Invalid email", "red" )
+
+main =
+    validateEmail "thedude@rubix.com"
+        |> Debug.toString
+        |> Html.text
